11cee0304107a5242c9c322e5e95de3a599150d2
Kikyou Akino <bellflower@web4u.jp>
2019-04-10 07:23:13 +0900

    Update to yespowerr16 support for Yenten 3.1.0

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -11,7 +11,7 @@ void yespowerr8_hash(const char *input, char *output, uint32_t len) {
 }
 
 void yespowerr16_hash(const char *input, char *output, uint32_t len) {
-    static const yespower_params_t params = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
+    static const yespower_params_t params = {YESPOWER_1_0, 4096, 16, NULL, 0};
     yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
 }
 
